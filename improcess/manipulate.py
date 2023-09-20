c4ec06f58d74a72b4efdb7992e376033f3988bcf
Tom Esposito <espos13@gmail.com>
2023-09-20 13:43:57 -0700

    Import: manipulate.py to do things like stack images (work in progress)

---
+++ improcess/manipulate.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+
+import os
+import pdb
+import numpy as np
+from glob import glob
+import matplotlib.pyplot as plt
+from matplotlib.colors import SymLogNorm
+from astropy.io import ascii, fits
+from scipy.ndimage import gaussian_filter, median_filter
+from scipy.ndimage import filters, map_coordinates
+from ..utils import make_radii
+
+
+def stack_images(imPaths, errorPaths=[], mode='weightedMean', negToNan=False,
+                 downweightNeg=False, maskRadii=None):
+    """
+    Combinate images by stacking using one of multiple methods given with
+    the 'mode' argument.
+    
+    negToNan: float
+        Pixels in individual images below this value will befilled with NaN.
+        False (default) will not fill anything.
+    mode: str
+        Options: ['weightMean' (default), 'simpleMean'].
+    maskRadii: list
+        Radii within which to mask images listed in imPaths during the
+        combination. Use 0 (zero) to indicate no masking.
+    """
+
+    data = []
+    hdrs = []
+    err = []
+    hdrs_err = []
+
+    for ff in imPaths:
+        with fits.open(ff) as hdul:
+            data.append(hdul[0].data)
+            hdrs.append(hdul[0].header)
+
+    for ii, ff in enumerate(errorPaths):
+        if ff is not None:
+            if ff == 'auto':
+                ff = os.path.join(os.path.split(imPaths[ii])[0],
+                                  'error_' + os.path.split(imPaths[ii])[1])
+            with fits.open(ff) as hdul:
+                err.append(hdul[0].data)
+                hdrs_err.append(hdul[0].header)
+        else:
+            try:
+                exp_total_first = hdrs[0]['TEXPTIME'] # [s]
+                exp_total_ii = hdrs[ii]['TEXPTIME'] # [s]
+                exp_ratio = np.sqrt(exp_total_first/exp_total_ii)
+                errScaled = exp_ratio*err[0]
+                errScaled[np.isnan(err[0])] = 1e-12
+                err.append(errScaled)
+            except:
+                err.append(1e-12*np.ones(data[ii].shape))
+            hdrs_err.append({})
+
+    data = np.array(data)
+    err = np.array(err)
+
+    # dataFilled = data[0].copy()
+    # for im in data:
+    #     dataFilled[(np.isnan(dataFilled)  & ~np.isnan(im))] = im[(np.isnan(dataFilled) & ~np.isnan(im))]
+    # Fill negative values below the provided negToNan level with NaN.
+    if negToNan:
+        for ii in range(len(data)):
+            data[ii][data[ii] < negToNan] = np.nan
+        print(f"Filled pixels valued below {negToNan} with NaN")
+
+    # Give minimal weight to negative-valued pixels.
+    if downweightNeg:
+        err[data < 0] *= 1e3
+
+    simpleMean = np.nanmean(data, axis=0)
+
+# FIX ME!!! Need to implement error-weighted average here.
+    if mode == 'simpleMean':
+        stack = simpleMean
+    else:
+        if maskRadii is not None:
+            for ii, rad in enumerate(maskRadii):
+                if rad > 0:
+                    cen = np.array([hdrs[ii].get('PSFCENTY', data[ii].shape[0]//2),
+                                    hdrs[ii].get('PSFCENTX', data[ii].shape[1]//2)])
+                    radii = make_radii(data[ii], cen)
+                    data[ii][radii <= rad] = np.nan
+                    # Smooth the edges of the weighting so there isn't a sharp
+                    # discontinuity at the mask boundary.
+                    errReweight = 10*(((rad+3) - radii[(radii >= rad) & (radii <= rad+3)])/3.)
+                    errReweight[errReweight <= 0] = 1e-6
+                    err[ii][(radii >= rad) & (radii <= rad+3)] *= errReweight
+                    err[ii][radii < rad] = np.nan
+
+        pixAveraged = len(data) - np.sum(np.array([np.isnan(im).astype(float) for im in data]), axis=0)
+        inverse_var_sum = np.nansum(1./(err**2), axis=0)
+        avg_img = np.nansum(data/(err**2), axis=0) / inverse_var_sum
+        # Put NaN back in where everything was NaN before.
+        avg_img[np.sum(np.isnan(data), axis=0) == data.shape[0]] = np.nan
+
+        stack = avg_img
+
+        # Work through the image weighting math.
+        err_map1 = err[0]
+        err_map2 = err[1]
+        # img1 / err_map1**2 / (1 / err_map1**2 + 1 / err_map2**2)
+        # img1 / err_map1**2 / (err_map2**2 / (err_map1**2 * err_map2**2) + err_map1**2 / (err_map1**2 * err_map2**2))
+        # img1 / err_map1**2 / ((err_map1**2 + err_map2**2) / (err_map1**2 * err_map2**2))
+        # img1_weighted = img1 * (err_map1**2 * err_map2**2) / (err_map1**2 * (err_map1**2 + err_map2**2)) # correct units of [intensity]
+
+        # err_nonan = np.nan_to_num(err, 1.)
+
+# TEMP!!!
+        # # weight1 = (err_map1**2 * err_map2**2) / (err_map1**2 * (err_map1**2 + err_map2**2))
+        # # weight2 = (err_map1**2 * err_map2**2) / (err_map2**2 * (err_map1**2 + err_map2**2))
+
+        # Propagate NaNs properly in weight maps.
+        # # weight1 should == 1 where wedge image is NaN (close in)
+        # # weight1 = np.sqrt(np.nanprod([err_map1**2, err_map2**2], axis=0) / np.nanprod([err_map1**2, np.nansum([err_map1**2, err_map2**2], axis=0)], axis=0)) # [unitless]
+        # # weight2 = np.sqrt(np.nanprod([err_map1**2, err_map2**2], axis=0) / np.nanprod([err_map2**2, np.nansum([err_map1**2, err_map2**2], axis=0)], axis=0)) # [unitless]
+        # weight1 = np.sqrt(np.nanprod([err_map1**2, err_map2**2], axis=0) / np.nanprod([err_map1**2, (err_map1**2 + err_map2**2)], axis=0)) # [unitless]
+        # weight2 = np.sqrt(np.nanprod([err_map1**2, err_map2**2], axis=0) / np.nanprod([err_map2**2, (err_map1**2 + err_map2**2)], axis=0)) # [unitless]
+
+        # # err_avg = np.sqrt(np.nansum([err_map1**2, err_map2**2], axis=0))/(pixAveraged**0.5)
+        # err_avg_unweighted = np.sqrt(np.nansum([err_map1**2, err_map2**2], axis=0))/(pixAveraged**0.5)
+        # err_avg = np.sqrt(np.nansum([(err_map1*weight1)**2, (err_map2*weight2)**2], axis=0))/(pixAveraged**0.5)
+
+    plt.figure(30)
+    plt.clf()
+    plt.imshow(stack[924:1124, 924:1124],
+               norm=SymLogNorm(linthresh=0.1, linscale=1., vmin=0, vmax=80))
+    plt.title(f"Stacked image {mode}")
+    plt.draw()
+    
+    plt.figure(31)
+    plt.clf()
+    plt.imshow(simpleMean[924:1124, 924:1124],
+               norm=SymLogNorm(linthresh=0.1, linscale=1., vmin=0, vmax=80))
+    plt.title("Simple mean")
+    plt.draw()
+
+    stackPath = os.path.splitext(imPaths[0])[0] + f'_{mode}_stack{len(imPaths)}.fits'
+    outHdr = hdrs[0].copy()
+    outHdr['FILETYPE'] = 'Averaged image'
+    outHdr['PROPAPER'] = 'MULTIPLE'
+    del outHdr['COMMENT']
+    outHdr['COMBOTYP'] = (mode, "Type of image combination used")
+    for ii, ff in enumerate(imPaths):
+        outHdr.add_comment(f"Constituent image {ii} = {os.path.split(ff)[-1]}")
+    outHdr.add_comment(f"Average type = {mode}")
+    outHdr.add_comment(f"Outermost radius masked around star (by image) = {maskRadii}")
+    fits.writeto(stackPath, stack, header=outHdr, overwrite=True)
+
+    print(f"Stacked image saved to {stackPath}")
+
+    # plt.show()
+
+    # pdb.set_trace()
+
+    # plt.close()
+
+    return
