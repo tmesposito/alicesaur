04171946e8126ca7c2c5126e9daf91fe50109797
Tom Esposito <espos13@gmail.com>
2024-05-15 16:01:20 -0700

    feat: Add CTI and distortion correction code
    - Add CTI class to handle Charge-Transfer Inefficiency correction.
    - Add functions in distortion.py to correct for geometric
    distortion.
    - Add distortion map IDC table file in FITS format

---
+++ calibration/cti.py
@@ -0,0 +1,448 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 09:51:08 2024
+
+To do Charge Transfer Inefficiency correction on HST images.
+
+Based originally on STScI's tutorial notebook at https://github.com/spacetelescope/STIS-Notebooks/tree/main/drizpac_notebook
+Based afterwards on modifications by Bin Ren <>.
+
+"""
+
+
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+
+# Load packages
+import os
+import sys
+import glob
+import shutil
+import subprocess
+import numpy as np
+from tempfile import TemporaryDirectory
+from astropy.io import fits
+
+# STIS packages
+from stis_cti import stis_cti, archive_dark_query
+from stis_cti import __version__ as stis_cti_version
+
+# STScI packages
+# from drizzlepac import tweakreg
+# from drizzlepac import astrodrizzle as ad
+from astroquery.mast import Observations
+
+# Custom package, copy_files.py should be in the same directory
+import copy_files as cf
+
+from alicesaur.utils import check_mkdir
+
+
+class CTI():
+    """
+    Class for Charge-Transfer Inefficiency correction.
+    """
+    
+    def __init__(self, *args, **kwargs):
+        """
+        Constructor of the CTI class.
+        """
+        
+        print (f'stis_cti v{stis_cti_version}')
+        
+        # # Check astroquery Observations options
+        # Observations.get_metadata("observations")
+        # Observations.get_metadata("products")
+        
+        self.obsMode = None
+        
+        # Define attributes with kwargs items
+        for key, val in kwargs.items():
+            setattr(self, key, val)
+
+
+    def check_mkdir(self, new_dir):
+        """Check for and make directory.
+        
+        Function to check for a directory and make it if it doesn't exist.
+        
+        Parameters
+        ----------
+        new_dir : str
+            Path of directory to check for/make.
+        
+        """
+        
+        # Check for directory
+        if os.path.exists(new_dir):
+            print('Directory exists: {}'.format(new_dir))
+        else:
+            # Make directory
+            os.makedirs(new_dir, 0o774)
+            print('Created directory: {}'.format(new_dir))
+            
+        return
+
+
+    def setup_directories(self, base_dir=None):
+
+        # Get the current working directory to handle relative paths
+        cwd = os.getcwd()
+
+        if base_dir is None:
+            self.base_dir = os.path.abspath(cwd)
+        else:
+            self.base_dir = os.path.abspath(base_dir)
+
+        # Set temporary data download store
+        self.cache_dir = os.path.join(self.base_dir, 'data_cache')
+
+        # Set the reference file directory
+        self.ref_dir = os.path.join(self.base_dir, 'reference_files')
+
+        # Set root and data directories
+        self.root_dir = os.path.join(self.base_dir, 'drizpac')
+        self.dat_dir = os.path.join(self.root_dir, 'data')
+        self.ccd_dir = os.path.join(self.dat_dir, 'ccd_data')
+        # mama_dir = os.path.join(dat_dir, 'mama_data')
+
+        # Set directories for CTI correction of CCD data
+        self.cti_dir = os.path.join(self.dat_dir, 'ccd_cti')
+        self.science = os.path.join(self.cti_dir, 'science')
+        self.darks = os.path.join(self.cti_dir, 'darks')
+        self.ref_cti = os.path.join(self.cti_dir, 'ref')
+
+        # Set directories for alignment with tweakreg
+        self.ccd_twk = os.path.join(self.dat_dir, 'ccd_twk')
+        # nuv_twk = os.path.join(dat_dir, 'nuv_twk')
+        # fuv_twk = os.path.join(dat_dir, 'fuv_twk')
+
+        # Set directories for drizzling with astrodrizzle
+        self.ccd_drz = os.path.join(self.dat_dir, 'ccd_drz')
+        # nuv_drz = os.path.join(dat_dir, 'nuv_drz')
+        # fuv_drz = os.path.join(dat_dir, 'fuv_drz')
+        # Check for directories and make them if they don't exist
+        # for d in [cache_dir, ref_dir, root_dir, dat_dir, ccd_dir, mama_dir, cti_dir, science, darks, ref_cti, ccd_twk, fuv_twk, nuv_twk, ccd_drz, fuv_drz, nuv_drz]:
+        for d in [self.cache_dir, self.ref_dir, self.root_dir, self.dat_dir, self.ccd_dir, self.cti_dir, self.science, self.darks, self.ref_cti, self.ccd_twk, self.ccd_drz]:
+            check_mkdir(d)
+
+        return
+
+
+    def setup_env(self):
+        
+        # Set environment variables
+        os.environ['CRDS_SERVER_URL'] = 'https://hst-crds.stsci.edu'
+        os.environ['CRDS_PATH'] = os.path.abspath(self.ref_dir)
+        os.environ['oref'] = os.path.join(os.path.abspath(self.ref_dir), 'references', 'hst', 'stis') + os.path.sep   # Trailing slash important
+        # print('oref: {}'.format(os.environ['oref']))
+        
+        return
+
+
+# FIX ME!!! Handle case of same target being observed in multiple blocks
+# separated by significant time within a single PID. Could take an input date
+# to specify which block to fetch, otherwise all will get rolled together.
+    def get_oids(self, pids, target_name='HD-114082'):
+        """
+        Get observation IDs from proposal IDs.
+
+        From a list of input proposal IDs (``pids``) get a list of observation IDs 
+        (``oids``) using astroquery.
+
+        Parameters
+        ----------
+        pids : list or array_like of str or int
+            List or array of proposal IDs to find observation IDs for
+
+        Returns
+        -------
+        oids : list of str
+            List of observation IDs within the input proposal IDs
+
+        """
+
+        oids = []
+
+        # For each PID get obs IDs
+        for pid in pids:
+            # Only get observations from the given Proposal ID taken with
+            # STIS/CCD in MIRVIS filter that are "image" type and calibrated
+            # to a certain level (3).
+            obs = Observations.query_criteria(proposal_id='{}'.format(pid),
+                                              instrument_name='STIS/CCD',
+                                              filters='MIRVIS',
+                                              dataproduct_type='image',
+                                              calib_level=3)
+            # obs columns: obs_id is filename identifier, whereas obsid is a
+            # numerical id roughly (but not necessarily) in order of execution.
+            # calib_level is 3 for normal images, t_min and t_max are start and
+            # end time of exposure.
+            # obs columns to ignore: em_min and em_max seem useless (all NaN),
+            # dataproduct_type (already specified),
+            # t_min, obs_collection (all HST), provenance_name (all CALSTIS),
+            # proposal id (we already specified), t_obs_release (don't care),
+            # obs_title (don't care), s_region, sequence_number (all masked),
+            # srcDen, 
+            # Sort observations by t_min to roughly sort by date recorded.
+            obs.sort('t_min')
+            # Get the list of data products associated with the obs.
+            products = Observations.get_product_list(obs)
+            # Separate out obs for only PSF reference stars.
+            obs_calPSF = obs[obs['target_classification'] == 'CALIBRATION;POINT SPREAD FUNCTION']
+            # obs_calPSF['instrument_name', 'filters', 'wavelength_region', 'target_name', 'target_classification', 'obs_id', 'obsid'].pprint(max_width=-1)
+            if target_name is not None:
+                obs_target = obs[obs['target_name'] == target_name]
+                # Get obs_id's for this target's images only.
+                oids_for_target = list(np.unique(obs_target['obs_id']))
+                t_start_target = min(obs_target['t_min'])
+                t_end_target = max(obs_target['t_max'])
+                # Locate PSF reference stars observed in the middle of the target obs.
+                # If none, look immediately before and after the target obs.
+                # Make sure to match the camera settings.
+                obs_calPSF_match = obs_calPSF[(obs_calPSF['t_min'] >= t_start_target) & (obs_calPSF['t_min'] <= t_end_target)]
+                print(f"\nFound {len(obs_calPSF_match)} PSF reference star images amid science images")
+                print(f"PSF reference stars found: {list(np.unique(obs_calPSF_match['target_name']))}")
+                # Get obs_id's for the PSF reference images.
+                oids_for_refs = list(np.unique(obs_calPSF_match['obs_id']))
+                # Combine all target and ref oids into one list.
+                oids = oids_for_target
+                oids += oids_for_refs
+                # Try to identify reference images by timing if they are not
+                # labeled via target_classification.
+                # Assume any target observed between the first and last target
+                # image are for a related PSF reference star.
+                if len(oids_for_refs) == 0:
+                    obs_amidst = obs[(obs['t_min'] >= t_start_target) & (obs['t_max'] <= t_end_target)]
+                    unique_target_names = np.unique(obs_amidst['target_name'])
+                    assumed_ref_names = unique_target_names[unique_target_names != target_name]
+                    ref_match = [ob in assumed_ref_names for ob in obs_amidst['target_name']]
+                    obs_ref_match = obs_amidst[ref_match]
+                    oids_for_refs_more = list(np.unique(obs_ref_match['obs_id']))
+                    oids += oids_for_refs_more
+                    # obs_ref_match = obs_amidst[obs_amidst['target_name'] in ]
+                # for oid in oids_for_target:
+                #     product_row = products[products['obs_id'] == oid]
+                #     # if product_row is not None:
+                #     #     print(product_row['obsID', 'obs_id', 'productFilename', 'parent_obsid'])
+                # # oids += list(np.unique(products['obs_id']))
+            else:
+                oids += list(np.unique(products['obs_id']))
+
+        # Weed out duplicate oids.
+        oids = np.unique(oids).tolist()
+
+        print('{} observation IDs found for {} proposal IDs:'.format(len(oids), len(pids)))
+        print(oids)
+
+        return oids, products, obs
+
+
+    def download_data(self, ids, destination, product_types=None,
+                      cache_dir=None, download_mode='general'):
+        '''Downloads MAST data products into a flattened location.
+        
+        Downloads data products (``product_type``) from input observation IDs (``ids``) 
+        from MAST and copies them to a single directory (``destination``) from the 
+        temporary download directory (``cache_dir``). Similar to stisteam.getdata(). 
+        Written by Sean Lockwood.
+        
+        Parameters
+        ----------
+        ids : list of str
+            List of observation IDs to download data products from
+        destination : str
+            Full path to final destination directory for data
+        product_types : list of str, optional
+            Names of product types to download for each observation ID (default is None, 
+            means all data products will be downloaded)
+        cache_dir : str, optional
+            Full path to temporary data download storage directory (default 
+            ``cache_dir`` as defined above)
+        
+        '''
+
+        if cache_dir is None:
+            cache_dir = self.cache_dir
+
+        assert os.path.isdir(destination), 'Destination must be a directory'
+        
+        print('\nDownloading & copying data to {}\n'.format(destination))
+    
+        # Get data products for each observation ID
+        obs = Observations.query_criteria(obs_id=ids)    
+        products = Observations.get_product_list(obs)
+        if product_types is not None:
+            products = products[[x.upper() in product_types for x in products['productSubGroupDescription']]]
+        
+        # Download data and combine into the destination directory
+        with TemporaryDirectory(prefix='downloads', dir=cache_dir) as d:
+            dl = Observations.download_products(products, mrp_only=False, download_dir=d)
+            for filename in dl['Local Path']:
+                # Ignore FITS with wrong occulter position.
+                with fits.open(filename, mode='readonly') as hdu:
+                    hdr = hdu[0].header
+                    occulter = hdr.get('PROPAPER', '')
+                    if (download_mode == 'science') and \
+                            (self.obsMode is not None) and \
+                            (occulter != self.obsMode.upper()):
+                        print(f"Ignoring {os.path.basename(filename)} for "\
+                              f"non-matching occulter ({occulter})")
+                        continue
+                shutil.copy(filename, destination)
+
+
+    # Find and update the headers with the best reference files and download them
+    def download_ref_files(self, image_list):
+        print(f'Downloading reference files for:\n{image_list}\n')
+        images = ' '.join(image_list)
+        subprocess.check_output('crds bestrefs --files {} --sync-references=1 --update-bestrefs'.format(images),
+                                shell=True, stderr=subprocess.DEVNULL)
+
+
+    def download_darks(self):
+        """
+        Determine dark exposures required for CCD data and download them (~1.2 GB)
+        """
+        needed_darks = archive_dark_query(glob.glob(os.path.join(self.science, '*_raw.fits')))
+        # needed_darks = archive_dark_query(glob.glob(os.path.join(self.science, '*_flt.fits')))
+
+        dark_exposures = set()
+        for anneal in needed_darks:
+            for dark in anneal['darks']:
+                dark_exposures.add(dark['exposure'])
+
+        self.download_data(dark_exposures, self.darks,
+                           product_types={'FLT', 'EPC', 'SPT'})
+        print("Finished downloading dark data\n")
+
+
+    def make_working_copies(self, exts=['*_raw.fits', '*_epc.fits', '*_spt.fits', '*_asn.fits', '*_flt.fits', '*_crj.fits']):
+        """
+        exts: Set file extensions for CCD image data to copy
+        """
+        
+        # Copy over all CCD files for each extension to the CTI correction directory
+        # os.chdir(cwd)
+        for ext in exts:
+            cf.copy_files_check(self.ccd_dir, self.science, files=f'{ext}')
+
+
+    def run_cti(self, pids, target_name=None, clean=True):
+
+        # Get the obs_id values from the PIDs.
+        self.pids = pids
+        self.ccd_oids, products, obs = self.get_oids(pids, target_name=target_name)
+
+        # Download data.
+        # Choices for product_types: 'RAW', 'FLT', 'CRJ', 'EPC', 'SPT', 'ASN', 'SX2'
+        self.download_data(self.ccd_oids, self.ccd_dir, product_types={'RAW'},
+                           download_mode='science')
+        print("Finished downloading CCD data\n")
+
+        # Download reference files.
+        # # Check in the Notebook directory
+        # os.chdir(cwd)
+        # Get a list of images for the CCD and download reference files
+        self.ccd_list = glob.glob(os.path.join(self.ccd_dir, '*_raw.fits'))
+        # self.ccd_list = glob.glob(os.path.join(self.ccd_dir, '*_flt.fits'))
+        # ccd_list = [fp.replace('flt.fits', 'raw.fits') for fp in tmp_list]
+        self.download_ref_files(self.ccd_list)
+        print("Finished downloading reference files\n")
+
+        # Copy files for processing.
+        self.make_working_copies()
+
+        self.download_darks()
+        # Run the CTI correction.
+        stis_cti(science_dir=os.path.abspath(self.science),
+                 dark_dir=os.path.abspath(self.darks),
+                 ref_dir=os.path.abspath(self.ref_cti),
+                 num_processes=15,
+                 verbose=2,
+                 clean=True)
+                 # clean_all= True)
+
+        self.post_cti_checks()
+        
+        # Copy CTI-corrected FITS files to the main data directory.
+        cf.copy_files_check(self.science, self.base_dir, files='*_flc.fits')
+
+        if clean:
+            self.cleanup()
+
+        return
+
+    def post_cti_checks(self):
+
+        # Print information for each file, check those that have been CTI corrected
+        # os.chdir(cwd)
+        # files = glob.glob(os.path.join(os.path.abspath(science), '*raw.fits'))
+        files = glob.glob(os.path.join(os.path.abspath(self.science), '*flt.fits'))
+        files.sort()
+
+        psm4 = 0
+        ampd = 0
+        ampo = 0
+        for f in files:
+
+            # Open file header
+            hdr = fits.getheader(f, 0)
+
+            # Get year obs
+            yr = int(hdr['TDATEOBS'].split('-')[0])
+
+            # Get file info
+            if yr <= 2004: 
+                print('\n...PRE-SM4 ({}), NOT CTE CORRECTED...'.format(yr))
+                psm4 += 1
+            # elif os.path.exists(f.replace('raw.fits', 'cte.fits')):
+            elif os.path.exists(f.replace('flt.fits', 'flc.fits')):
+                print('\n***CTE CORRECTED, AMP {} ({})***'.format(hdr['CCDAMP'], yr))
+                ampd += 1
+            else: 
+                ampo += 1
+                print('\n~NON-CTE CORRECTED, AMP {} ({})~'.format(hdr['CCDAMP'], yr))
+            print('FILE: {}, PID: {}, ROOT: {}'.format(os.path.basename(f), hdr['PROPOSID'], hdr['ROOTNAME']))
+            print('INST: {}, DETECTOR: {}, AP: {}'.format(hdr['INSTRUME'], hdr['DETECTOR'], hdr['APERTURE']))
+            print('DATE OBS:{}, PROCESSED: {}'.format(hdr['TDATEOBS'], hdr['DATE']))
+
+        # Run checks on data
+        nraw = len(glob.glob(os.path.join(self.science, '*raw.fits')))
+        ncte = len(glob.glob(os.path.join(self.science, '*cte.fits')))
+        print(f'{nraw} raw CCD files input, {ncte} CTE corrected files output')   # Should be equal if all data taken on amp D and post-SM4
+        print(f'{psm4} pre-SM4, {ampd} amp D (CTE corr), {ampo} other amp (non-CTE)')
+
+    def cleanup(self):
+        
+        # Delete cache directories.
+        if os.path.isdir(self.cache_dir):
+            try:
+                shutil.rmtree(self.cache_dir)
+                print("\nDeleted CTI cache directories")
+            except Exception as ee:
+                print("\nWARNING: FAILED to deleted CTI cache directories at "\
+                      f"{self.cache_dir}")
+                print(ee)
+        # Delete CTI data directories.
+        if os.path.isdir(self.root_dir):
+            try:
+                shutil.rmtree(self.root_dir)
+                print("Deleted CTI temporary data directories")
+            except Exception as ee:
+                print("\nWARNING: FAILED to deleted CTI temporary data "\
+                      f"directories at {self.root_dir}")
+                print(ee)
+        # # Delete CTI reference file directories.
+        # if os.path.isdir(self.ref_dir):
+        #     try:
+        #         shutil.rmtree(self.ref_dir)
+        #         print("Deleted CTI reference file directories\n")
+        #     except Exception as ee:
+        #         print("\nWARNING: FAILED to deleted CTI reference file "\
+        #               f"directories at {self.ref_dir}\n")
+        #         print(ee)
+
+        return
+        
---
+++ calibration/distortion.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 13 11:19:14 2023
+
+@author: tom
+"""
+import os
+import shutil
+from distutils import spawn
+# from drizzlepac import astrodrizzle
+# from drizzlepac.stisData import STISInputImage
+from stistools.x2d import x2d
+
+
+def correct_distortion(imgPathList, outputPaths="", refDir=None, inst='stis',
+                       overwrite=False):
+
+    if type(outputPaths) is str:
+        if os.path.isdir(outputPaths):
+            outputPaths = len(imgPathList)*[outputPaths]
+        else:
+            print("\nWARNING: outputPaths for correct_distortion can be only"\
+                  " a list of strings or a single str path to a directory."\
+                  " Forcing outputs to add a _x2d suffix to input image "\
+                  "paths.\n")
+            outputPaths = ""
+    if outputPaths == "":
+        outputPaths = len(imgPathList)*[""]
+
+    # Set environment path to IDC table file, if given manually.
+    if refDir is not None:
+        os.environ['oref'] = os.path.abspath(refDir)
+    else:
+        if os.environ.get('oref') is None:
+            # Must have trailing slash in 'oref' path.
+            default_oref_path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), '../data/distortion')), '')
+            os.environ['oref'] = default_oref_path
+            print("\nWARNING: Environment variable 'oref' was not defined"\
+                  " so distortion correction may fail. "\
+                  f"Resetting 'oref' to {default_oref_path}.\nYou may need"\
+                  " to set it manually to the path to the IDC table file's"\
+                  " parent directory or provide that path here with the"\
+                  " refDir argument.")
+
+    undistort_images(imgPathList, outputPaths=outputPaths, overwrite=overwrite)
+
+    return
+
+
+def undistort_images(imgPathList, outputPaths="", overwrite=False):
+
+    if overwrite:
+        print("WARNING: Will overwrite existing x2d.fits files")
+
+    for ii, imgPath in enumerate(imgPathList):
+        undistort_single_image(imgPath, outputPath=outputPaths[ii],
+                               overwrite=overwrite)
+
+    return
+
+
+def undistort_single_image(imgPath, outputPath="", overwrite=False):
+    """
+    Correct geometric distortion of a single STIS image.
+    
+    ***
+    TOM'S NOTES ON X2D: The stistools.x2d.x2d function is almost purely
+    a wrapper for calling the cs7.e executable. So we can easily replace our
+    dependence on x2d with a (simpler) wrapper of our own.
+    ***
+
+    Parameters
+    ----------
+    imgPath : TYPE
+        DESCRIPTION.
+    outputPath : TYPE, optional
+        DESCRIPTION. The default is "".
+    overwrite : TYPE, optional
+        DESCRIPTION. The default is False.
+
+    Returns
+    -------
+    None. Writes a new *_x2d.fits file with the undistorted image(s).
+
+    """
+
+    print(os.environ.get('oref'))
+
+    if os.environ.get('oref') is None:
+        print('\nWARNING: Environment variable "oref" is not defined'\
+              ' so distortion correction will likely fail. You may need'\
+              ' to set it manually as the path to the IDC table file'\
+              ' or provide that path here with the idcPath argument.\n')
+
+    # Check for presence of required IRAF executable named 'cs7.e'.
+    path_cs7e = spawn.find_executable('cs7.e')
+    if path_cs7e is None:
+        print('\nWARNING: CALSTIS executable cs7.e not found in your environment.'\
+              ' Distortion correction will not work without it.')
+    # If cs7.e is found, make sure it is in the environment path and add it if
+    # it is not.
+    else:
+        if os.path.dirname(path_cs7e) not in os.environ['PATH']:
+            print('\ncs7.e CALSTIS executable was not in the environment '\
+                  'PATH. Adding it.')
+            os.environ["PATH"] += os.pathsep + os.path.dirname(path_cs7e)
+        else:
+            pass
+        
+    # If overwriting existing files, keep a backup copy until the correction
+    # has finished successfully.
+    if overwrite:
+        if os.path.exists(outputPath):
+            try:
+                shutil.move(outputPath, outputPath + '-backup')
+            except:
+                print("\nWARNING: Could not back up existing "\
+                      f"distortion-corrected x2d.fits at {outputPath}\n")
+
+    # Do the distortion correction.
+    x2d_status = x2d(imgPath, output=outputPath, fluxcorr=False, helcorr=False)
+
+    if x2d_status == 0:
+        if overwrite:
+            if os.path.exists(outputPath + '-backup'):
+                os.remove(outputPath + '-backup')
+    elif x2d_status == 1:
+        print("\nHELP! The IRAF executable 'cs7.e' was not found to do"\
+              " the distortion correction. Please make sure it is in your"\
+              " environment's system path.")
+    elif x2d_status == 2:
+        print("\nHELP! The IRAF executable 'cs7.e' was not found to do"\
+              " the distortion correction. Please make sure it is in your"\
+              " environment's system path.")
+    elif x2d_status > 2:
+        print("\nWARNING! Something unknown went wrong during distortion"\
+              "correction.\n")
+
+    # Delete backup or move backup back to original depending on success.
+    if x2d_status == 0:
+        if overwrite:
+            if os.path.exists(outputPath + '-backup'):
+                os.remove(outputPath + '-backup')
+    else:
+        if overwrite:
+            if os.path.exists(outputPath + '-backup'):
+                shutil.move(outputPath + '-backup', outputPath)
+
+    return
+
+
+# def custom_x2d_cs7(fileList):
+    
+#     for (i, infile) in enumerate(infiles):
+
+#         arglist = ["cs7.e"]
+
+#         arglist.append(infile)
+#         if outfiles is not None:
+#             arglist.append(outfiles[i])
+
+#         if verbose:
+#             arglist.append("-v")
+#         if timestamps:
+#             arglist.append("-t")
+
+#         switch_was_set = False
+#         if helcorr == "perform":
+#             arglist.append("-hel")
+#             switch_was_set = True
+#         if fluxcorr == "perform":
+#             arglist.append("-flux")
+#             switch_was_set = True
+#         if not switch_was_set:
+#             arglist.append("-x2d")
+
+#         if err_alg:
+#             if err_alg == "wgt_err":
+#                 arglist.append("-wgt_err")
+#             elif err_alg != "wgt_var":
+#                 raise RuntimeError("err_alg must be either 'wgt_err'"
+#                                    " or 'wgt_var'; you specified '%s'" % err_alg)
+
+#         if blazeshift is not None:
+#             arglist.append("-b")
+#             arglist.append("%.10g" % blazeshift)
+
+#         if verbose:
+#             print("Running x2d on {}".format(infile))
+#             print("  {}".format(arglist))
+#         status = subprocess.call(arglist, stdout=fd_trailer,
+#                                  stderr=subprocess.STDOUT)
+#         if status:
+#             cumulative_status = 1
+#             if verbose:
+#                 print("Warning:  status = {}".format(status))
+    
+    
