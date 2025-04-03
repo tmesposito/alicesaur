fix (major): Intensity calibration, auto star masking, Gaia MCMC
- Correct the intensity calibration to units of COUNTS S^-1.
Previously, the unified images were being normalized by only the
exposure time of a single CRSPLIT. Now, they are normalized by the
total integration time of the image (the summed exposure times of
all CRSPLITS) to match the way the unified images conserve the
intensity in COUNTS.
- Fix auto star masking so it runs on final image and reference
PSF images.
- Actually apply the auto mask to the reference PSF during PSF
subtraction. This removes negative residuals in PSF-subtracted
science images caused by sources in the reference images, but also
opens the possibility that improper auto masking in the reference
image can mask pixels unnecessarily in the final image.
- Reduce amount of reference image's primary PSF that is masked
by the auto star masker. Do this by performing a "reverse PSF
subtraction" by subtracting a median of the science images from
the reference PSF image so we can locate and mask the stars in
the reference image without the primary PSF in the way.
- Pass more arguments to auto star masking function calls.
- Adjust Gaia astrometry MCMC prior and walker initialization to
avoid zombie walkers that are initialized outside the prior
bounds. Add 0 lower bounds for MCMC's x and y centers.
- Update Gaia PSF least-squares fitter with modern LMLSQFitter
class.
- Make Gaia overview star labels a little nicer.

gaia/astrometry.py
@@ -57,11 +57,15 @@ def lnlike(p, sky_pos, sky_cov, px_pos, px_cov, include_indx):
   
 def lnprior(p):
 
+    if (p[0] < 0):
+        return -np.inf
+    if (p[1] < 0):
+        return -np.inf
     if (p[2] <= 5) | (p[2] > 200.0):
         return -np.inf
     if (p[3] <= 5) | (p[3] > 200.0):
         return -np.inf
-    if (p[4] < 0.0) | (p[4] > (2.0*np.pi)):
+    if (p[4] < -2.1 *np.pi) | (p[4] > (2.1*np.pi)):
         return -np.inf
 
     return 0
@@ -82,6 +86,12 @@ def mcmc(sky_pos, sky_cov, px_pos, px_cov, include_indx, guess_x, guess_y, guess
     pos0 = np.zeros((nwalkers, ndim))
     for k in range(0, ndim):
         pos0[..., k] = np.random.normal(p0[k], dp[k], nwalkers)
+
+    # Sanitize initial positions by regenerating any that fail the prior.
+    for ii, p0_i in enumerate(pos0):
+        while np.isinf(lnprior(pos0[ii])):
+            for k in range(0, ndim):
+                pos0[ii] = np.random.normal(p0[k], dp[k])
 
     args = (sky_pos, sky_cov, px_pos, px_cov, include_indx)
 
@@ -271,8 +281,8 @@ def main(im_path, inst, target_id, target_rv, target_xy, gaia_catalogue='DR3',
         # Exclude sources near or outside the edges of the image array.
         if not ((20 <= x[i] <= s[1] - 20) and (20 <= y[i] <= s[0] - 20)):
             exclude_id += [source_id[i]]
-        # Exclude sources centered on masked pixels of the image array.
-        elif (np.isnan(im[y_int, x_int]) | (im[y_int, x_int] == 0.0)):
+        # Exclude sources vignetted by masked pixels of the image array.
+        elif (np.any(np.isnan(im[y_int-20:y_int+21, x_int-20:x_int+21])) | (im[y_int, x_int] == 0.0)):
             # TODO - check mask array
             exclude_id += [source_id[i]]
         else:
@@ -343,7 +353,9 @@ def main(im_path, inst, target_id, target_rv, target_xy, gaia_catalogue='DR3',
     nsteps = 100
     nburn = 25
 
-    samples, lnp, blobs = mcmc(sky_pos, sky_cov, px_pos, px_cov, include_indx, target_xy[0], target_xy[1], guess_ps, guess_tn, nsteps=nsteps)
+    samples, lnp, blobs = mcmc(sky_pos, sky_cov, px_pos, px_cov, include_indx,
+                               target_xy[0], target_xy[1], guess_ps, guess_tn,
+                               nsteps=nsteps)
 
     # Generate list of chi2, to remove outliers in a second run
     # Updated star center could be used as input for the second run

gaia/fit_psf.py
@@ -111,7 +111,7 @@ def fit(im, x, y, source_id, exclude_id, star_errors, offset, inflation,
                                                 tied = {'y_stddev': tie_std},
                                                 fixed = {'theta': True})
 
-                least_sq_fit = fitting.LevMarLSQFitter()
+                least_sq_fit = fitting.LMLSQFitter(calc_uncertainties=False)
                 yarr,xarr = np.indices(stamp2.shape)
                 best_fit_model = least_sq_fit(gauss_model, xarr, yarr, stamp2, weights=weight2)
 

gaia/gaia_plot.py
@@ -23,7 +23,8 @@ def plot_overview(im, x, y, source_id, exclude_id, outname='gaia_overview'):
             marker, mec = 'o', 'c'
 
         ax.plot(x[j], y[j], marker, mec=mec, ms=5, mew=0.5, mfc='None')
-        ax.annotate(str(j), xy=(x[j], y[j]), xycoords='data', color=mec, xytext=(4, 4), textcoords='offset points', fontsize=8)
+        ax.annotate(str(j), xy=(x[j], y[j]), xycoords='data', color=mec,
+                    xytext=(2, 2), textcoords='offset points', fontsize=7)
     
     fig.savefig('{}.png'.format(outname), dpi=300, bbox_inches='tight') # TODO: Change to meaningful name
     plt.close('all')
@@ -92,7 +93,7 @@ def plot_fits(data_table, include_indx, sky_pos, sky_cov, px_pos, px_cov,
         h, _, _, _ = ax[j][4].hist2d(pred_x, pred_y, cmap=cm.Greys, bins=(xbins, ybins))
         _ = ax[j][4].contour(gaia_utils.confidence_levels(h.T), [1.99, 2.99, 3.99], extent=[xbins.min(),xbins.max(),ybins.min(),ybins.max()])
 
-        ## And plot pixel measurement
+        # And plot pixel measurement
         _ = ax[j][4].errorbar(px_pos[i][0], px_pos[i][1], 
             xerr=np.sqrt(px_cov[i][0][0]),
             yerr=np.sqrt(px_cov[i][1][1]), fmt='o', color='red', ms=2.0, capsize=5)
@@ -100,6 +101,7 @@ def plot_fits(data_table, include_indx, sky_pos, sky_cov, px_pos, px_cov,
         _ = ax[j][4].xaxis.get_major_formatter().set_useOffset(False)
         _ = ax[j][4].yaxis.get_major_formatter().set_useOffset(False)
 
+        # Write the fits specs on the right of each row.
         try:
             _ = ax[j][5].annotate('{}'.format(data_table['SOURCE_ID'][i]), xy=(0.05, 0.9), xycoords='axes fraction', fontsize=6)
         except:

improcess/astrosniff.py
@@ -13,20 +13,25 @@ def load_fits_data(dataDir):
     """
     Loads the data from the final FITS image in the specified directory.
     """
-    for filename in os.listdir(dataDir):
+    for filename in sorted(os.listdir(dataDir)):
         if filename.startswith('final_') and filename.endswith('.fits'):
-            file_path = os.path.join(dataDir, filename)
-            with fits.open(file_path) as hdul:
-                data_cube = hdul[0].data
-                # If there are multiple data slices, use the first.
-                if data_cube.ndim == 3:
-                    data = data_cube[0]
-                else:
-                    data = data_cube
+            try:
+                file_path = os.path.join(dataDir, filename)
+                with fits.open(file_path) as hdul:
+                    data_cube = hdul[1].data
+                    # If there are multiple data slices, use the first.
+                    if data_cube.ndim == 3:
+                        data = data_cube[0]
+                    else:
+                        data = data_cube
+                    hdr = hdul[0].header
+                    star = np.array([hdr['PSFCENTY'], hdr['PSFCENTX']])
 
-            return data, filename
+                return data, filename, star
+            except:
+                continue
 
-    return None, None
+    return None, None, None
 
 def subtract_background(data):
     """
@@ -42,7 +47,7 @@ def subtract_background(data):
 
 def process_data(final_data, threshold):
     """
-    First step of the data processing using using convolve with gaussian kernel
+    First step of the data processing using convolution with gaussian kernel.
     """
     kernel = make_2dgaussian_kernel(3, size=7)
     convolved_data = convolve(final_data, kernel)
@@ -50,16 +55,29 @@ def process_data(final_data, threshold):
     seg_map.data[seg_map.data > 0] = 1
     return seg_map
 
-def exclude_center_from_segmentation(seg_map, data, exclude_center=(1024, 1024), 
+def exclude_center_from_segmentation(seg_map, data,
+                                     exclude_center=(1024, 1024),
                                      exclude_radius=120):
     """
     Exclude a certain circular region from the center to avoid masking possible
     disks.
+
+    Inputs:
+
+        exclude_radius: scalar
+          Radius from the exclude_center to avoid masking, in [pixels].
+
+    Outputs:
+
+        A 2-d array "segmentation map" with value 1 for all masked pixels and
+        0 for all unmasked pixels.
+
     """
     y, x = np.ogrid[:data.shape[0], :data.shape[1]]
     distance_from_center = np.sqrt((x - exclude_center[0])**2 + (y - exclude_center[1])**2)
     exclusion_mask = distance_from_center <= exclude_radius
     seg_map.data[exclusion_mask] = 0
+
     return seg_map
 
 def masked_pixels_coords(seg_map, csv_file_path='masked_pixel_yx.csv'):
@@ -73,27 +91,34 @@ def masked_pixels_coords(seg_map, csv_file_path='masked_pixel_yx.csv'):
 
     return df
 
-def save_seg_map_as_fits(seg_map, original_filename, output_directory='.'):
+def save_seg_map_as_fits(seg_map, input_filename, output_directory='.'):
 
-    output_file = f"segmap_{original_filename}"
+    output_file = f"segmap_{os.path.splitext(input_filename)[0]}.fits"
     output_path = os.path.join(output_directory, output_file)
     hdu = fits.PrimaryHDU(seg_map.data)
     hdu.writeto(output_path, overwrite=True)
     print(f"Segmentation map saved as {output_path}")
+    return
 
-def main_masking(data=None, dataDir='.', output_filename='image'):
+def main_masking(data=None, dataDir='.', input_filename='image',
+                 star=None, exclude_radius=120):
 
     if data is None:
-        data, original_filename = load_fits_data(dataDir=dataDir)
-        output_filename = original_filename
+        data, input_filename, star = load_fits_data(dataDir=dataDir)
 
     if data is not None:
+        print(f"Auto star masking: Using input image {input_filename}")
         final_data, threshold = subtract_background(data)
         seg_map = process_data(final_data, threshold)
-        seg_map = exclude_center_from_segmentation(seg_map, data)
-        csv_path = os.path.join(dataDir, f'masked_pixel_yx_{output_filename}.csv')
+        if star is not None:
+            seg_map = exclude_center_from_segmentation(seg_map, data,
+                                                 exclude_center=star,
+                                                 exclude_radius=exclude_radius)
+        # Save the coordinates of the masked pixels to a csv table.
+        csv_path = os.path.join(dataDir, f'masked_pixel_yx_{os.path.splitext(input_filename)[0]}.csv')
         masked_pixels_coords(seg_map, csv_path)
-        save_seg_map_as_fits(seg_map, output_filename,
+        # Save the mask to a FITS image.
+        save_seg_map_as_fits(seg_map, input_filename,
                              output_directory=dataDir)
         return seg_map
     else:

pipeline/pipeline.py
@@ -335,14 +335,17 @@ class Pipeline(object):
             self.logger.warning(f"*** NO FITS files found at {self.dataDir + '*_' + suffix +'.fits'} *** !!\n")
         
 
-    def make_star_mask(self, im=None, output_filename='image'):
+    def make_star_mask(self, im=None, input_filename='segmap',
+                       star=(1024, 1024), exclude_radius=120):
         """
         Execute astrosniff and return the mask 2D array.
         """
-        self.logger.info("Starting masking process...")
+        self.logger.info("Starting auto star masking process...")
         try:
             seg_map = astrosniff.main_masking(data=im, dataDir=self.dataDir,
-                                              output_filename=output_filename)
+                                              input_filename=input_filename,
+                                              star=star,
+                                              exclude_radius=exclude_radius)
             if seg_map is not None:
                 self.logger.info("Auto star masking process completed successfully.")
             else:
@@ -2546,18 +2549,28 @@ class Pipeline(object):
                                                 exclusions=exclusionsRef,
                                                 cen=self.alignStar, cenOffset=self.alignStarOffsets[ind],
                                                 paOffset=0, spikeAngles=self.spikeAngles)
-# TESTING!!!
+
                 # Add star mask to sourceMasks, rotated to the correct PA.
                 if not self.noAutoMask:
-                    # Run astrosniff to create the 2D mask array for each ref
-                    # image.
+                    # Run astrosniff to create the 2D auto star mask array for
+                    # each reference PSF image.
                     if not self.noAutoMask:
-                        ref_star_mask = self.make_star_mask(im=self.workingImgs[ind],
-                                                output_filename=os.path.splitext(os.path.basename(self.fileList[ind]))[0])
+                        # Do a "reverse PSF subtraction" by subtracting a
+                        # median of the science images from the reference PSF
+                        # image so we can locate and mask the stars in the
+                        # reference image without the primary PSF in the way.
+                        comboRefSciMasks = psfSubMasks[self.sciInds] | np.any(psfSubMasks[self.refInds], axis=0)
+                        ratio_ref_sci_0 = np.nansum(self.workingImgs[ind][~comboRefSciMasks[0]]) / np.nansum(np.nanmedian(self.workingImgs[self.sciInds], axis=0)[~comboRefSciMasks[0]])
+                        ref_star_subtracted = self.workingImgs[ind] - 1.1*np.clip(ratio_ref_sci_0*np.nanmedian(self.workingImgs[self.sciInds], axis=0), 0, np.inf)
+                        # Make the actual star mask here..
+                        ref_star_mask = self.make_star_mask(im=ref_star_subtracted,
+                                                input_filename=os.path.splitext(os.path.basename(self.fileList[ind]))[0] + '_refpsf',
+                                                star=self.alignStar,
+                                                exclude_radius=min((sub_r_out, 200)))
                         if ref_star_mask is not None:
                             # Reformat the output slightly to an ndarray.
                             ref_star_mask = np.array(ref_star_mask.data, dtype=bool)
-                            sourceMasks[ind] += ref_star_mask
+                            psfSubMasks[ind] += ref_star_mask
                         else:
                             self.logger.warning("Reference image auto star "\
                                                 "mask could not be made")
@@ -2804,6 +2817,7 @@ class Pipeline(object):
 
         # Perform astrometry on occulted primary using background star
         # Gaia positions.
+        all_gaia_out = []
         if self.do_gaia and (psfsubPath is not None):
             self.logger.info("*** Running GAIA ASTROMETRY on individual PSF-subtracted images ***\n")
             self.targSimbad = utils.format_target_name(self.targ)
@@ -2812,7 +2826,6 @@ class Pipeline(object):
                 if gaiaID is not None:
                     gaiaIDNumber = int(gaiaID.split(' ')[-1])
                     target_rv = [0., 0.]
-                    all_gaia_out = []
 
 # FIX ME!!! Still using the axc headers for Gaia astrometry below.
 
@@ -2994,6 +3007,7 @@ class Pipeline(object):
                 try:
                     gaiaID = get_gaia_id(self.targSimbad)
                     if gaiaID is not None:
+                        gaiaIDNumber = int(gaiaID.split(' ')[-1])
                         target_rv = [0., 0.]
                         # Retrieve the final image header containing WCS info.
                         finalHdr = fits.getheader(finalPath, ext=('SCI', 1))